07e97166f0ed842cf741e58467945af2b10e80e7
ianthm <ian.thomson@segmantics.com>
2018-08-04 14:26:02 +0100

    Update to solidity contract logic

--- TheBigRedBus/DecentralizedApp/samples/BigRedBusContract.sol
+++ TheBigRedBus/DecentralizedApp/samples/BigRedBusContract.sol
@@ -1,9 +1,53 @@
 contract BigRedBusContract {
-     address test;
+     	 
+	 //// Declare an event here
+	 
+	 //// Declare properties that uniquely identify an instance of an authorised game ////
+	 // uint gameID;
+	
+
+	 //// Declare data structure for a here ////
+	 // struct GamePerson {
+	 // uint personId;
+	 // string twitterHandle;
+	 // decimal long;
+	 // decimal lat;
+	 // binary imageLocation;
+	 // date timeLocation;
+	 
+
+	 //// Create an array of the people who will play a game ////
+	 //  GamePerson[] public gamepersons;
+
+
+	 //// Function 1 - Business logic to decide the person is allowed to play the game ////
+		// The person must be confirmed to be in the location he says he is as the gps matches image 
+		// There must be a least two real people to play a game at the same location
+		
+	 // Create a person for entry into blockchain and to enable Gamne app to commence
+
+
+	 ////Generate random data for the blockchain ////
+
+
+	 
+	 
+	 
+	 address test;
 
      mapping (address => uint) testState; 
 
      function helloWorld() {
+
+	 // add properties here 
+	 // twitter
+
+
+
+
+
+
+
 		// implement function here
      }
 }
