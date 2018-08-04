e77a4f9000e9f7a57e1bc641d65105371076bab3
Segmantics <ian.thomson@segmantics.com>
2018-08-04 15:05:34 +0100

    Logic for a Solidity Contract - First Draft

--- TheBigRedBus/DecentralizedApp/samples/BigRedBusContract.sol
+++ TheBigRedBus/DecentralizedApp/samples/BigRedBusContract.sol
@@ -1,53 +1,40 @@
 contract BigRedBusContract {
+
+// EVENT
      	 
-	 //// Declare an event here
+//// Declare an event here ///
+
+	 event NewPerson(string twitterHandle, decimal longitude, decimal latitude, binary imageLocation);   // upper camel and camel
 	 
-	 //// Declare properties that uniquely identify an instance of an authorised game ////
-	 // uint gameID;
+// PROPERTIES
+
+//// Declare properties that uniquely identify an instance of an authorised game ////
+
+	 uint gameID; 
+        int personsProximity;
+
 	
+//// Declare data structure for a here ////
 
-	 //// Declare data structure for a here ////
-	 // struct GamePerson {
-	 // uint personId;
-	 // string twitterHandle;
-	 // decimal long;
-	 // decimal lat;
-	 // binary imageLocation;
-	 // date timeLocation;
+	 struct Person {
+	 uint personId;
+	 string twitterHandle;
+	 decimal long;
+	 decimal lat;
+	 binary imageLocation;
+	 date timeLocation;
 	 
 
-	 //// Create an array of the people who will play a game ////
-	 //  GamePerson[] public gamepersons;
+//// Create an array of the people who will play a game ////
+	 Person[] public persons;
 
+// FUNCTION ( LOGIC )
 
-	 //// Function 1 - Business logic to decide the person is allowed to play the game ////
-		// The person must be confirmed to be in the location he says he is as the gps matches image 
-		// There must be a least two real people to play a game at the same location
-		
-	 // Create a person for entry into blockchain and to enable Gamne app to commence
+//// Business logic to decide the person is allowed to play the game ////
+// Function 1 > The person must be confirmed to be in the location area he says he is as the gps matches image
+// Function 2 > Check the location tolerance is acceptable 
+// Function 3 > There must be a least two real people to play a game at the same location
+// Function 4 > If these conditions are met them start the game
+// Function 5 > If the game is started then write to blockchain
 
-
-	 ////Generate random data for the blockchain ////
-
-
-	 
-	 
-	 
-	 address test;
-
-     mapping (address => uint) testState; 
-
-     function helloWorld() {
-
-	 // add properties here 
-	 // twitter
-
-
-
-
-
-
-
-		// implement function here
-     }
 }
